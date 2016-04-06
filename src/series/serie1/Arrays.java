25bba7b3ef9eea0313e443ecbafcf189852a7a4c
Baltas93 <d.carvalho@live.com.pt>
2016-04-06 16:41:16 +0100

    20160406_1641

--- src/series/serie1/Arrays.java
+++ src/series/serie1/Arrays.java
@@ -3,24 +3,29 @@ package series.serie1;
 public class Arrays {
 
     public static int removeIndexes(int v[], int l, int r, int[] vi, int li, int ri) {
-        int indV =l , indVi=li;
-        while(indV <=r && indVi<=ri){
-            if(indV==vi[indVi]){
-                swap(indV,v,r);
-                r--;
-                indVi++;
-            }
-            indV++;
+        if(v.length<=0||vi.length<=0) return v.length;
+        if(v==null||vi==null) return v.length;
+        int indV =r , indVi=ri;
+        while(vi[indVi]>indV&& indVi>=li){ //reduz indice do sub-Array vi até o seu conteudo ser o mais proximo possivel de indice v
+            indVi--;
         }
-        return r-l;
+        while(indV >=l && indVi>=li){
+            if(indV==vi[indVi]) {
+                swap(indV, v, r);
+                r--;
+                indVi--;
+            }
+            indV--;
+        }
+        return r-l+1;
     }
 
     private static void swap(int ind, int[] v, int r) {
         int aux= v[ind], i;
-        for(i=ind;i<=r;i++){
+        for(i=ind;i<r;i++){
             v[i]=v[i+1];
         }
-        v[i+1]=aux;
+        v[i]=aux;
     }
 
 //    public static int countEquals(int[] v1, int l1, int r1, int[] v2, int l2, int r2){
@@ -28,9 +33,42 @@ public class Arrays {
 //    }
 
     public static String greaterCommonPrefix( String[] v, int l, int r, String word) {
-        throw new UnsupportedOperationException();
+       if(v.length==0)return null;
+        int greaterInd=r,greaterSize=0,curr=l,currSize=0;
+        while(curr<r){
+            if((currSize=prefixSize(word,v[curr]))>=greaterSize){
+                greaterSize=currSize;
+                greaterInd=curr;
+            }
+            else{
+                return v[greaterInd];
+            }
+            curr++;
+        }
+        if(greaterSize== 0) return v[r];
+        return v[greaterInd];
     }
-    
+
+    private static int prefixSize(String word, String s) {
+        int len=0;
+        if(word.length()<=s.length()){
+            len=word.length();
+        }
+        else{
+            len=s.length();
+        }
+        int count=0;
+        while(count<len){
+            if(word.charAt(count)==s.charAt(count)){
+                count++;
+            }
+            else {
+                return count;
+            }
+        }
+        return  count;
+    }
+
 
     public static int[] getTheKElementsNearestX(int[] v, int l, int r, int x, int k){
         throw new UnsupportedOperationException();
